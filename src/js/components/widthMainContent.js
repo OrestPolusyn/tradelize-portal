3dac3f00e0dbd07219caab1da1d83d460302fda8
Orest <opect.pol@i.ua>
2021-11-23 14:20:00 +0200

    srart search page

---
+++ src/js/components/widthMainContent.js
@@ -0,0 +1,10 @@
+let contentWidth = document.querySelector('.main'),
+  asideMenuWidth = document.querySelector('.aside-nav');
+
+window.addEventListener('resize', mainContentWidth);
+mainContentWidth();
+
+function mainContentWidth() {
+  contentWidth.style.paddingLeft = asideMenuWidth.clientWidth + 'px';
+
+}
