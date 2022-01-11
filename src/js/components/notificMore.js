cb215a10f6db6af206e33d55517b2f90f1b3d0a1
Orest <opect.pol@i.ua>
2022-01-11 14:43:18 +0200

    add not

--- src/js/components/notificMore.js
+++ src/js/components/notificMore.js
@@ -1,4 +1,5 @@
 const moreNotifBtn = Array.from(document.querySelectorAll('.notifications-content-more-btn'))
+const notItems = Array.from(document.querySelectorAll('.notifications-content-item'))
 
 if (moreNotifBtn.length > 0) {
   moreNotifBtn.forEach(element => {
@@ -10,6 +11,42 @@ if (moreNotifBtn.length > 0) {
       })
 
       currentBtn.nextElementSibling.classList.add('notifications-more-dropdown-show')
+
     })
+  })
+}
+
+if (notItems.length > 0) {
+  notItems.forEach(element => {
+    const popcorn = element.querySelector('.popcorn')
+    const tooltip = element.querySelector('.popdown')
+
+    if (popcorn && tooltip) {
+      Popper.createPopper(popcorn, tooltip, {
+        placement: 'auto-start',
+        modifiers: {
+          flip: {
+            boundariesElement: 'scrollParent',
+            flipVariations: true
+          },
+          name: 'offset',
+          options: {
+            offset: [-20, 10],
+          },
+          preventOverflow: {
+            boundariesElement: 'scrollParent',
+            escapeWithReference: true
+          },
+        }
+      })
+
+      const scrollToCenter = () => {
+        const scrollParent = document.querySelector('.notifications-content');
+        scrollParent.scrollTop = popcorn.offsetTop - scrollParent.clientHeight / 2
+        scrollParent.scrollLeft = popcorn.offsetLeft + popcorn.clientWidth / 2 - scrollParent.clientWidth / 2
+      }
+
+      scrollToCenter()
+    }
   })
 }
